136cccf70304dd04d479ed4de437ccd05d673ef9
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-06-06 06:14:40 -0300

    Manipulando classe Date

---
+++ src/data_hora/test/ClasseDate.java
@@ -0,0 +1,20 @@
+package data_hora.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ClasseDate {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Date hoje = new Date();
+        Date data = sdf1.parse("21/12/1989");
+
+        System.out.println("hoje: " + sdf1.format(hoje));
+        System.out.println("Hoje com horas: " + sdf2.format(hoje));
+        System.out.println("Data: " + sdf2.format(data));
+
+    }
+}
