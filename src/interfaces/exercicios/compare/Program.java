1aaf61464117f1335a4afa3a198bb963020a71a8
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-07-08 06:09:28 -0300

    Finalização do exercício de fixação sobre interfaces e arquivos

--- src/interfaces/exercicios/compare/Program.java
+++ src/interfaces/exercicios/compare/Program.java
@@ -7,30 +7,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import interfaces.exercicios.compare.entities.Employee;
+
+
 public class Program {
 
     public static void main(String[] args) {
         
-        List<String> list = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
         String path = "/home/maicon-cardoso/Documentos/in.txt";
 
         try (BufferedReader buffer = new BufferedReader(new FileReader(path))) {
 
-            String name = buffer.readLine();
-            while (name != null) {
-                list.add(name);
-                name = buffer.readLine();
+            String employeeCsv = buffer.readLine();
+            while (employeeCsv != null) {
+                String[] fields = employeeCsv.split(",");
+                String name = fields[0];
+                double salary = Double.parseDouble(fields[1]);
+                list.add(new Employee(name, salary));
+
+                employeeCsv = buffer.readLine();
             }
 
             Collections.sort(list);
-            for (String s : list) {
-                System.out.println(s);
+            for (Employee emp : list) {
+                System.out.println(emp.getName() + ", " + emp.getSalary());
             }
             
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        
-        
     }
 }
