952263c95bf62932f599b883f3c21ad9fd278e68
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-07-16 06:42:30 -0300

    Introdução e análise de fluxo de bytes

---
+++ src/arquivos/test/book/ReadBytes.java
@@ -0,0 +1,16 @@
+package arquivos.test.book;
+
+import java.io.IOException;
+
+public class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        
+        byte[] data = new byte[10];
+
+        System.out.println("Enter some characters.");
+        int numRead = System.in.read(data);
+        System.out.print("You entered: ");
+        for (int i=0; i<numRead; i++) 
+            System.out.print((char) data[i]);
+    }
+}
---
+++ src/arquivos/test/book/WriteDemo.java
@@ -0,0 +1,14 @@
+package arquivos.test.book;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        
+        int b;
+
+        b = 'X';
+
+        System.out.write(b);
+        System.out.write('\n');
+
+    }
+}
