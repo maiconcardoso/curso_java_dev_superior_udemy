cdf846541f7fd86dc06302770f80fb8a049db455
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-07-16 12:32:02 -0300

    Refatoração do programa ShowFile

--- src/arquivos/test/book/ShowFile.java
+++ src/arquivos/test/book/ShowFile.java
@@ -5,47 +5,42 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /*
- * Exibe um arquivo de texto.
- * 
- * Para usar este programa, especifique o nome
- * do arquivo que deseja ver.
- * Por exemplo, para ver um arquivo chamado TEST.txt,
- * use a linha de comando abaixo.
- * 
- * java ShowFile TEST.txt
+ * Esta variação enapsula o código que abre e 
+ * acessa o arquivo dentro do mesmo bloco try.
+ * O arquivo é fechado pelo bloco finally.
 */
 
 public class ShowFile {
     public static void main(String[] args) {
         
         int i;
-        FileInputStream fin;
+        FileInputStream fin = null;
 
         // Primeiro verifica se um arquivo foi especificado.
-
         if (args.length != 1) {
-            System.out.println("Usage: ShowFile File" );
+            System.out.println("Usage: ShowFile Filename" );
             return;
         }
+
+        // o código a seguir abre um arquivo, lê caracteres até EOF
+        // ser alcançado e então fecha o arquivo via um bloco finally
         try {
             fin = new FileInputStream(args[0]);
+            do {
+                i = fin.read();
+                if (i != -1) System.out.print((char) i);
+            } while (i != -1);
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found.");
             return;
-        }
-
-        try {
-            // lê bytes até o EOF ser alcançado.
-            do {
-                i = fin.read();
-                if (i != -1) 
-                    System.out.print((char) i);
-            } while (i != -1);
         } catch (IOException e) {
-            System.out.println("Error reading file.");
+            System.out.println("An I/O Error Occurred.");
         } finally {
+
+            // Fecha o arquivo em todos os casos
             try {
-                fin.close();
+                if (fin != null)
+                    fin.close();
             } catch (IOException e) {
                 System.out.println("Error closing file.");
             }
