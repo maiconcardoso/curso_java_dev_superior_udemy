dbae12cf233479a06fa337e4b2678d8317a7d432
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-07-18 06:50:24 -0300

    Analisando classes de fluxo de caracteres

--- src/arquivos/test/book/fluxoCaracteres/ReadChars.java
+++ src/arquivos/test/book/fluxoCaracteres/ReadChars.java
@@ -1,5 +1,22 @@
 package arquivos.test.book.fluxoCaracteres;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ReadChars {
 
+    public static void main(String[] args) throws IOException {
+        char c;
+        BufferedReader br = new 
+            BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Enter characters, period to quit.");
+
+        // lê caracteres
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        }while(c != '.');
+    }
 }
---
+++ src/arquivos/test/book/fluxoCaracteres/ReadLines.java
@@ -0,0 +1,22 @@
+package arquivos.test.book.fluxoCaracteres;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadLines {
+    public static void main(String[] args) throws IOException {
+        // Cria um bufferedReader usando System.in
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+
+        System.out.println("Enter lines of text.");
+        System.out.println("Enter 'stop' to quit.");
+
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while (!str.equals("stop"));
+    }
+}
