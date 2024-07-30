2f69e7b0398059bb6f86e437edb3c59cd5cdf460
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-07-30 06:47:01 -0300

    Finalização da função que recebe outra função como argumento

--- src/programacao_funcional/test/app/Program.java
+++ src/programacao_funcional/test/app/Program.java
@@ -19,7 +19,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        double sum = ps.filteredSum(list);
+        double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
 
         System.out.printf("Soma: %.2f%n", sum);
 
